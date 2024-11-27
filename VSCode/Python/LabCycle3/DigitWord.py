2ca91254f48ce97f76acdfcdc3e7b28a4807fe31
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2024-11-27 11:10:56 +0530

    Added LabCycle3 Prgm

---
+++ VSCode/Python/LabCycle3/DigitWord.py
@@ -0,0 +1,24 @@
+N = input("Enter a number: ")
+for ch in N:
+  match ch:
+    case '1':
+      print("One", end=" ")
+    case '2':
+      print("Two", end=" ")
+    case '3':
+      print("Three", end=" ")
+    case '4':
+      print("Four", end=" ")
+    case '5':
+      print("Five", end=" ")
+    case '6':
+      print("Six", end=" ")
+    case '7':
+      print("Seven", end=" ")
+    case '8':
+      print("Eight", end=" ")
+    case '9':
+      print("Nine", end=" ")
+    case '0':
+      print("Zero", end=" ")
+print()
