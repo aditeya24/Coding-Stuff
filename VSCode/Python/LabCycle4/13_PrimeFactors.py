84e35c639bc10b8ac0c748d104bbe85a705ea202
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2024-12-03 21:35:49 +0530

    Added missing code

---
+++ VSCode/Python/LabCycle4/13_PrimeFactors.py
@@ -0,0 +1,17 @@
+from math import sqrt
+def isPrime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True 
+
+n = int(input("Enter a number: "))
+factors = set()
+for i in range(1, int(sqrt(n)) + 1):
+    if n % i == 0:
+        factors.add(i)
+        factors.add(n // i)
+f = (sorted(factors))
+
--- VSCode/Python/LabCycle4/3_RightTriangle.py
+++ VSCode/Python/LabCycle4/3_RightTriangle.py
@@ -1,1 +1,13 @@
+def isRightTriangle(a,b,c):
+    side = [a,b,c]
+    side.sort()
+    if side[2] ** 2 == side[0] ** 2 + side[1] ** 2:
+        return True
+    else:
+        return False
 
+a = float(input("Enter side 1: "))
+b = float(input("Enter side 1: "))
+c = float(input("Enter side 1: "))
+
+print("Is right triangle" if isRightTriangle(a,b,c) else "Is not right triangle")
