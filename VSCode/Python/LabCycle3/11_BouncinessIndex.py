2ab41a6ecfa0aa91c4c0041a3b47036a35e58ca6
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2024-12-04 11:31:01 +0530

    Fixed error

--- VSCode/Python/LabCycle3/11_BouncinessIndex.py
+++ VSCode/Python/LabCycle3/11_BouncinessIndex.py
@@ -2,8 +2,9 @@ h = int(input("Enter initial height in feet: "))
 bi = float(input("Enter bounciness index: "))
 n = int(input("Enter number of bounces: "))
 total = h
-current_height = h * bi
+current_height = h
 for i in range(0,n):
-    total += 2 * current_height
     current_height *= bi
+    total += 2 * current_height
+total -= current_height
 print(f"Total distance travelled: {total:.2f} feet")
