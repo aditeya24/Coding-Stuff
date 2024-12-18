5db1d7ee19e5e7739cb6f6183a3946357b08b501
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2024-12-18 09:19:35 +0530

    Update 16_WordSort.py

--- VSCode/Python/LabCycle5/16_WordSort.py
+++ VSCode/Python/LabCycle5/16_WordSort.py
@@ -1,7 +1,6 @@
 N = int(input("Enter N: "))
 print("Input words: ")
 words = [input() for i in range(N)]
-#words_dict = {i: len(words[i]) for i in range(N)}
 words_sorted = sorted(words)
 words_length = [len(words_sorted[i]) for i in range(N)]
 for i in range(N):
