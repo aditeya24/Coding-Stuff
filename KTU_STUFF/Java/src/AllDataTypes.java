d6c02edfcc45cb9bb526109c3efe5d1cda3ce55c
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-07-17 12:58:10 +0530

    Added random basic programs

---
+++ KTU_STUFF/Java/src/AllDataTypes.java
@@ -0,0 +1,14 @@
+public class AllDataTypes {
+    public static void main(String[] args) {
+        int a=10;
+        float b=3.14f;
+        double c=123.456;
+        char d='A';
+        boolean e=true;
+        byte f=100;
+        short g=30000;
+        long h=1234567890L;
+
+        System.out.println(a+","+b+","+c+","+d+","+e+","+f+","+g+","+h);
+    }
+}
